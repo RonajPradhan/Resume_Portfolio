50045988b2f6e21874c52b8ab52dd897d2038d9d
Ronaj Pradhan <78714906+FunkyBuddha741@users.noreply.github.com>
2024-05-16 19:08:54 -0400

    Re-did the original website into my personal protfolio

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,71 +1,74 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { CgCPlusPlus } from 'react-icons/cg';
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
+	DiJavascript1,
+	DiReact,
+	DiNodejs,
+	DiMongodb,
+	DiPython,
+	DiGit,
+	DiJava,
+} from 'react-icons/di';
 import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+	SiRedis,
+	SiFirebase,
+	SiNextdotjs,
+	SiSolidity,
+	SiMysql,
+	SiSpring,
+	SiRedux,
+	SiIntellijidea,
+} from 'react-icons/si';
+import { FaSass, FaAws } from 'react-icons/fa';
 
 function Techstack() {
-  return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+	return (
+		<Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
+			{/* <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
-    </Row>
-  );
+      </Col> */}
+			<Col xs={4} md={2} className="tech-icons">
+				<DiJavascript1 />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<FaSass />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiNodejs />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiReact />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiSpring />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiRedux />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiNextdotjs />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiGit />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiFirebase />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<FaAws />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiMysql />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiPython />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiJava />
+			</Col>
+		</Row>
+	);
 }
 
 export default Techstack;
